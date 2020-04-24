22ba410a538257b862a393a1da32a8de768449dc
wonderix <wonderix@googlemail.com>
2020-04-24 11:59:18 +0200

    Add shalm formula

--- shalm.rb
+++ shalm.rb
@@ -1,8 +1,8 @@
 class Shalm < Formula
   desc "K8s installation tool"
   homepage "https://github.com/wonderix/shalm"
-  version "v0.5.6-2-gb081433"
-  url "https://github.com/wonderix/shalm/releases/download/v0.5.6-2-gb081433/shalm-binary-darwin.tgz"
+  version "v0.5.6"
+  url "https://github.com/wonderix/shalm/releases/download/v0.5.6/shalm-binary-darwin.tgz"
   sha256 "20c62a1974bdd139a6a0a2cd77b919d4f55e50deb5e20369e82d657e7fc4693f"
 
   depends_on :arch => :x86_64
