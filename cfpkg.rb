0f37b1a85b9bc8c6121ea15b07d4a2eeb9b31bb2
GitHub Action <wonderix@googlemail.com>
2020-05-08 13:46:58 +0000

    Bump to newest cfpkg release

--- cfpkg.rb
+++ cfpkg.rb
@@ -1,13 +1,13 @@
-class Shalm < Formula
+class Cfpkg < Formula
   desc "K8s installation tool"
   homepage "https://github.com/wonderix/cfpkg"
   version "v0.1.0"
   if OS.mac?
     url "https://github.com/wonderix/cfpkg/releases/download/v0.1.0/cfpkg-binary-darwin.tgz"
-    sha256 "917b7fbfff8a11d1987e07142f0a853d02a0b76d3b4b9acaec67a24855bc7941"
+    sha256 "403b6c02b479d40481f9e8ab10c8f16985207566016a845cda26a53f1bf0925e"
   elsif OS.linux?
     url "https://github.com/wonderix/cfpkg/releases/download/v0.1.0/cfpkg-binary-linux.tgz"
-    sha256 "f397c2cd78eee64c94ed74e7dd180f2d605e9bae500aac49aa6a2f2acf6ac221"
+    sha256 "db7eb87a6e52709646efd25922bdff4ceb4e3ca0ff91bf4630091c2c9eca072a"
   end
 
   depends_on :arch => :x86_64
