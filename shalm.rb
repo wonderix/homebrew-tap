e62429b06c8be89c5474f824b6623cce5637e8e1
GitHub Action <wonderix@googlemail.com>
2020-09-08 07:04:31 +0000

    Bump to newest shalm release

--- shalm.rb
+++ shalm.rb
@@ -1,13 +1,13 @@
 class Shalm < Formula
   desc "K8s installation tool"
   homepage "https://github.com/wonderix/shalm"
-  version "v0.6.2"
+  version "v0.6.3"
   if OS.mac?
-    url "https://github.com/wonderix/shalm/releases/download/v0.6.2/shalm-binary-darwin.tgz"
-    sha256 "ac3b72dedfd643b4a9f39a838da209d9a0244e41079ef6d8c392db6c5a02f835"
+    url "https://github.com/wonderix/shalm/releases/download/v0.6.3/shalm-binary-darwin.tgz"
+    sha256 "a8bb1412f6c806aecfe643574572a1b7ad6325caf732987ae9f12d8a9f89955a"
   elsif OS.linux?
-    url "https://github.com/wonderix/shalm/releases/download/v0.6.2/shalm-binary-linux.tgz"
-    sha256 "17e8c604ab747f98ef9e811dd09fd3b56cc938dc769726c4f9d88db8203ec5f4"
+    url "https://github.com/wonderix/shalm/releases/download/v0.6.3/shalm-binary-linux.tgz"
+    sha256 "6bb3ed7965460bcc7a310122beab71920f25ee5bf6f3ac6562d1e391b8672938"
   end
 
   depends_on :arch => :x86_64
